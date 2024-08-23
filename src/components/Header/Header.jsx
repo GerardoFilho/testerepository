75d492459160934bc43861fd59d618a3b019caa1
Gerardo Filho <gerardo_filho95@hotmail..com>
2024-08-22 21:03:00 -0300

    teste

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='thiago'>
+    <div>Header</div>
+    <div>Thiago lindo</div>
+    <button className='oi'>thiago</button>
+    </div>
+  )
+}
+
+export default Header
